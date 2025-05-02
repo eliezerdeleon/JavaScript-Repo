335a4d3af2cef8327bf73bd823f9cc428d8707ec
eliezerdeleon <djnb.nickyblast@gmail.com>
2025-05-02 11:55:09 -0400

    case naming

--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -9,3 +9,13 @@ console.log(firstName, lastName);
 var age = 30;
 
 console.log(age);
+
+//Naming conventions
+// Only letters, numbers, underscores and dollar signs
+// Cant start with a number
+
+// Multi-word formatting
+// firstName = camelCase
+// first_name = underscore
+// FirstName = PascalCase
+// firstname = lowercase
