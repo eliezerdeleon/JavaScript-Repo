8439032ce34148724d7733c34912fa233d408ad8
Eliezer De Leon <djnb.nickyblast@gmail.com>
2025-05-05 14:00:07 -0400

    primitive data types

--- 01-variables-data-types/03-variables/script.js
+++ 01-variables-data-types/03-variables/script.js
@@ -6,9 +6,9 @@ let lastName = 'Doe';
 
 console.log(firstName, lastName);
 
-var age = 30;
+var age1 = 30;
 
-console.log(age);
+console.log(age1);
 
 //Naming conventions
 // Only letters, numbers, underscores and dollar signs
@@ -22,9 +22,9 @@ console.log(age);
 
 // Re-assigning variables
 
-age = 31;
+age1 = 31;
 
-console.log(age);
+console.log(age1);
 
 let score;
 
@@ -36,7 +36,7 @@ if (true) {
   score = score = 1;
 }
 
-const x = 100;
+const x1 = 100;
 
 const arr = [1, 2, 3, 4];
 arr.push(5);
@@ -60,3 +60,25 @@ const d = 1,
 console.log(d);
 console.log(a);
 
+//primitive data types
+// string, number, boolean, null, undefined, symbol
+// reference data types
+// arrays, objects, functions
+// string
+const name = 'John Doe';
+const age = 30;
+const isCool = true;
+const rating = 4.5;
+const x = null;
+const y = undefined;
+const z = Symbol('a random symbol');
+const hobbies = ['movies', 'music'];
+const address = {
+  city: 'Boston',
+  state: 'MA',
+};
+const today = new Date();
+console.log(today);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isCool);
