475eb339cb404908e3bf9cfa34ab4c1e2977f5bf
eliezerdeleon <djnb.nickyblast@gmail.com>
2025-05-07 17:55:45 -0400

    operators

---
+++ 01-variables-data-types/07-operators/script.js
@@ -0,0 +1,53 @@
+//1. Arithmetic Operators
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+//Concatenation
+x = 'hello' + ' ' + 'world';
+
+//Exponent
+x = 2 ** 3;
+
+//Increment
+x = 1;
+
+x = x + 1;
+x++;
+
+//Decrement
+x = x - 1;
+x--;
+
+// Assignment Operators
+x = 10;
+
+x += 5;
+x = x + 1;
+x -= 5;
+x *= 5;
+x / 5;
+x %= 5;
+x **= 5;
+
+//3.Comparison Operators
+
+x = 2 == 2;
+
+x = 2 === 2;
+
+x = 2 != 2;
+
+x = 2 !== 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
