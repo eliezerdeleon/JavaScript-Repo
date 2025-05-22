fdfa7a4ee6359b560499e13e585d4d4752e15e16
Eliezer De Leon <djnb.nickyblast@gmail.com>
2025-05-22 19:15:32 -0400

    Math Object 1

--- 01-variables-data-types/12-math-object/script.js
+++ 01-variables-data-types/12-math-object/script.js
@@ -1,1 +1,21 @@
-console.log(Math);
+let x;
+
+x = Math.sqrt(16);
+
+x = Math.abs(-5);
+
+x = Math.round(4.6);
+
+x = Math.ceil(4.2);
+
+x = Math.floor(4.9);
+
+x = Math.pow(2, 3);
+
+x = Math.min(1, 2, 3, 4, 5);
+
+x = Math.max(1, 2, 3, 4, 5);
+
+x = Math.floor(Math.random() * 10 + 1);
+
+console.log(x);
